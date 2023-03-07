26f92c5d590e4051a184c768ccab41c7f0f848d6
Renan Zanesco Comoli <37589475+enedois@users.noreply.github.com>
2023-03-07 21:14:59 +1000

    add links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Create from './components/Create';
+import Read from './components/read';
+import Update from './components/update';
+import { ReactDOM } from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (   
+    
+    <div className="main">
+      <h2 className="main-header">React Crud Operations</h2>
+      <ul>
+        <li><a href="/create">Create</a></li>
+        <li><a href="/read">Read</a></li>
+        <li><a href="/update">Update</a></li>
+        </ul>
+      <div>
+        
+        <Router>
+        <Routes>
+          <Route path="/create" element={<Create/>}/>
+          <Route exact path='/read' element={<Read/>} />
+          <Route path='/update' element={<Update/>} />
+        </Routes> 
+        </Router>
+
+        {/* <Create/> */}
+            
+       
+      </div>
     </div>
+ 
+
   );
 }
 
---
+++ src/components/read.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import { Table, Button } from 'semantic-ui-react'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+
+
+export default function Read() {
+
+    const [APIData, setAPIData] = useState([]);
+    useEffect(() => {
+        axios.get(`https://63f735ffe40e087c95899b68.mockapi.io/fakeData`)
+            .then((response) => {
+                setAPIData(response.data);
+            })
+    }, [])
+
+    const setData = (data) => {
+        console.log(data);
+         let { id, firstName, lastName, checkbox } = data;
+        localStorage.setItem('ID', id);
+        localStorage.setItem('First Name', firstName);
+        localStorage.setItem('Last Name', lastName);
+        localStorage.setItem('Checkbox Value', checkbox)
+     }
+
+     const onDelete = (id) => {
+        axios.delete(`https://63f735ffe40e087c95899b68.mockapi.io/fakeData/${id}`).then(() => {getData();})
+      }
+
+      const getData = () => {
+        axios.get(`https://63f735ffe40e087c95899b68.mockapi.io/fakeData`)
+            .then((getData) => {
+                 setAPIData(getData.data);
+             })
+    }
+
+    return (
+        <div>
+            <Table singleLine>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Name</Table.HeaderCell>
+                        <Table.HeaderCell>Registration Date</Table.HeaderCell>
+                        <Table.HeaderCell>E-mail address</Table.HeaderCell>
+                        <Table.HeaderCell>Premium Plan</Table.HeaderCell>
+                        <Table.HeaderCell>Update</Table.HeaderCell>
+                        <Table.HeaderCell>Delete</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    {APIData.map((data) => {
+                        return (
+                            <Table.Row>
+                                <Table.Cell>{data.firstName}</Table.Cell>
+                                <Table.Cell>{data.lastName}</Table.Cell>
+                                <Table.Cell>{data.checkbox ? 'Checked' : 'Unchecked'}</Table.Cell>
+                                <Link to='/update'><Table.Cell><Button onClick={() => setData(data)}>Update</Button></Table.Cell>
+                                </Link>
+                                <Table.Cell><Button onClick={() => onDelete(data.id)}>Delete</Button></Table.Cell>
+                            </Table.Row>
+                        )
+                    })}
+                </Table.Body>
+            </Table>
+        </div>
+    )
+}
